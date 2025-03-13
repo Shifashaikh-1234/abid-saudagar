6b867428a1fc596ff9b9aebe02f32fdf250aee9f
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-13 12:27:05 +0530

    Update NavbarLogo.jsx

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -4,7 +4,7 @@ const NavbarLogo = () => {
         <h1 className="text-white text-2xl sm:hidden md:block">
           <img src='https://res.cloudinary.com/dlqldp4wi/image/upload/v1741516559/abidLogo_t1ti9w.jpg' alt="logo" class="h-[60px] ml-5 rounded-full"/>
         </h1>
-        <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+        <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:mr-3 sm:block">
         <img src='https://res.cloudinary.com/dlqldp4wi/image/upload/v1741516559/abidLogo_t1ti9w.jpg' alt="logo" class="h-[50px] ml-4 rounded-full"/>
         </h1>
       </div>
