01cb67098c130f1f5ecf646b7b1d37bb398b5845
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-13 12:29:15 +0530

    Update NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,7 +3,7 @@ import { LuArrowDownRight } from "react-icons/lu";
 
 const NavbarBtn = () => {
   return (
-    <button className=" px-4 py-2 rounded-full text-xl font-bold mr-3 font-body text-black  flex items-center gap-1  bg-beige transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
+    <button className=" px-4 py-2 rounded-full text-xl font-bold mr-3 ml-3 font-body text-black  flex items-center gap-1  bg-beige transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
       <Link spy={true} smooth={true} duration={500} offset={-120} to="contact" className="sm:text-md lg:text-xl">
         Meeting
       </Link>
