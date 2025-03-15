38c2e0985990b3c858c713ff9fe3667901bddfc8
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-15 14:15:14 +0530

    Update ContactMeRight.jsx

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -1,11 +1,11 @@
 import ContactInfo from "./ContactInfo";
 const ContactMeRight = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-12 ">
+    <div className="flex flex-col items-center justify-center gap-8 lg:mr-8 lg:ml-0 ">
       <img
         src="https://res.cloudinary.com/dlqldp4wi/image/upload/v1741516559/abidLogo_t1ti9w.jpg"
         alt="email image"
-        className="max-w-[250px] rounded-full mr-6 lg:block sm:hidden"
+        className="max-w-[250px] rounded-full mr-8 lg:block sm:hidden"
       />
       <ContactInfo />
     </div>
