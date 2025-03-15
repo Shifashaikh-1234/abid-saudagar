96984c6f017441f9a43d66412606ba32a14de6a8
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-15 14:16:24 +0530

    Update ContactInfo.jsx

--- src/components/contactMeSection/ContactInfo.jsx
+++ src/components/contactMeSection/ContactInfo.jsx
@@ -10,7 +10,7 @@ import SingleInfo from "./SingleInfo";
 
 const ContactInfo = () => {
   return (
-    <div className="flex flex-col gap-4 sm:mt-0  sm:mb-5 sm:ml-6 sm:pb-3 text-black font-body">
+    <div className="flex flex-col gap-4 sm:mt-0 lg:ml-0 lg-mr-8 sm:mb-5 sm:ml-6 sm:pb-3 text-black font-body">
       <a href="https://mail.google.com/mail/?view=cm&fs=1&to=sendmailtoAbid@gmail.com" target="_blank" rel="noopener norefferer"><SingleInfo text="sendmailtoAbid@gmail.com" Image={HiOutlineMail} /></a>
       <a href="https://calendly.com/abidsaudagar/free-15-minute-ai-search-consulting"><SingleInfo text="Abid Saudagar" Image={SiCalendly} /></a>
       <a href="https://www.linkedin.com/in/abidsaudagar/"><SingleInfo text="Abid Saudagar" Image={FaLinkedinIn}/></a>
