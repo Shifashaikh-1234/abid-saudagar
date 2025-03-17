a633fa7873b80537e1946f75265bc2db8c96f8b4
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-17 14:11:38 +0530

    Update HeroImage.jsx

--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
     <div className="relative self-end h-full w-full items-center justify-center sm:mt-4">
       <div className=" h-full w-full ">
         <img
-          src="https://res.cloudinary.com/dlqldp4wi/image/upload/v1741534283/abidImg_xlybki.png"
+          src="https://res.cloudinary.com/dlqldp4wi/image/upload/v1742194171/image_1_dc409n.png"
           alt="abid saudagar"
           className="w-auto h-auto md:max-w-[570px] border-x-darkGrey  sm:max-w-[250px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
         />
