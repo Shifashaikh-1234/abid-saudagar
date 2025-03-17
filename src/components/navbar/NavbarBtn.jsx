30ebbf81d106552d08757722102084c078597817
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-17 20:11:01 +0530

    Update NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -5,16 +5,16 @@ const NavbarBtn = () => {
      <div>
     
     <div className="mr-6 inline-flex items-center space-x-2 sm:hidden font-body font-bold lg:flex">
-    <p className="text-md font-body text-black whitespace-nowrap">Press</p>
+    <p className="text-md font-body text-white whitespace-nowrap">Press</p>
   
-    <img src="https://res.cloudinary.com/dlqldp4wi/image/upload/v1742201069/da4klgpw_ocfevb.png" alt="S key" className="h-[45px] w-13  mix-blend-darken rounded-md shadow-orange"/>
-    <p className="text-md font-body text-black whitespace-nowrap">to Schedule Calendly Meeting </p>
+    <img src="https://res.cloudinary.com/dlqldp4wi/image/upload/v1742201069/da4klgpw_ocfevb.png" alt="S key" className="h-[45px] w-13 rounded-md shadow-orange"/>
+    <p className="text-md font-body text-white whitespace-nowrap">to Schedule Calendly Meeting </p>
     </div>
     
     
     < div className="mr-6 flex flex-row lg:hidden sm:block font-body font-bold">
-     <p className="text-md  font-body text-black whitespace-nowrap">Schedule</p>
-     <p className="text-md  text-black font-body whitespace-nowrap">Meeting</p>
+     <p className="text-md  font-body text-white whitespace-nowrap">Schedule</p>
+     <p className="text-md  text-white font-body whitespace-nowrap">Meeting</p>
      </div>
      
      </div>
