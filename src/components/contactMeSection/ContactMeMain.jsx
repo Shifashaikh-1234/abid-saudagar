f62295a6ab682696cb4668a5bf6e6d211877bde5
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-19 12:33:01 +0530

    Update ContactMeMain.jsx

--- src/components/contactMeSection/ContactMeMain.jsx
+++ src/components/contactMeSection/ContactMeMain.jsx
@@ -8,7 +8,7 @@ const ContactMeMain = () => {
       className="max-w-[1000px] mx-auto items-center justify-center mt-[100px] px-4 "
     >
       <h2 className="text-6xl text-darkBlueM mb-10 text-center font-body">Contact</h2>
-      <div className="flex justify-between gap-24 bg-ashM p-6 rounded-2xl lg:flex-row sm:flex-col">
+      <div className="flex justify-between gap-20 bg-ashM p-6 rounded-2xl lg:flex-row sm:flex-col">
         <ContactMeLeft />
         <ContactMeRight />
       </div>
