4f7c58532861dad0f951b51d54ae68445f95758a
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-19 15:12:14 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -13,7 +13,7 @@ const HeroPic = () => {
       <img
         src="https://res.cloudinary.com/dlqldp4wi/image/upload/v1742194171/image_1_dc409n.png"
         alt="Abid saudagar"
-        className=" mix-blend-multiply lg:w-[360px] h-auto sm:w-[280px] h-auto shadow-cyanShadow rounded-full"
+        className=" mix-blend-multiply mt-4 lg:w-[360px] h-auto sm:w-[280px] h-auto shadow-cyanShadow rounded-full"
       />
 
       <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
