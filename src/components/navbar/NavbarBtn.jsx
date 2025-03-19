c15f71e5e44bad2de1d6a5f467058245faa0b1c5
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-19 13:10:16 +0530

    Update NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,15 +3,16 @@
 const NavbarBtn = () => {
   return (
      <div>
-    
+    <a href="https://calendly.com/abidsaudagar/free-15-minute-ai-search-consulting" target="_blank" rel="noopener norefferer">
     <div className="mr-6 inline-flex items-center space-x-2 sm:hidden font-body font-normal lg:flex">
-      <a href="https://calendly.com/abidsaudagar/free-15-minute-ai-search-consulting" target="_blank" rel="noopener norefferer">
+      
     <p className="text-md font-body text-white whitespace-nowrap">Press</p>
   
     <img src="https://res.cloudinary.com/dlqldp4wi/image/upload/v1742201069/da4klgpw_ocfevb.png" alt="S key" className="h-[40px] w-11 rounded-md shadow-orange"/>
     <p className="text-md font-body text-white whitespace-nowrap">to Schedule Calendly Meeting </p>
-         </a>
+         
     </div>
+      </a>
     
     
     < div className="mr-6 flex flex-row lg:hidden sm:block font-body font-normal">
